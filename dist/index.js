f60feb9de43919dd98dcc5c102d1d17a5bd565d9
Rogerio Kotsubo <rogeriokotsubo@gmail.com>
2022-10-27 20:31:58 -0300

    features/v1

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,35 @@
 "use strict";
 class Validator {
     constructor(data) {
-        this._data = data;
+    }
+}
+class StringValidator extends Validator {
+    constructor(data) {
+        if (typeof data === 'string') {
+            super(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+class NumberValidator extends Validator {
+    constructor(data) {
+        if (typeof data === 'number') {
+            super(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
+    }
+}
+class BooleanValidator extends Validator {
+    constructor(data) {
+        if (typeof data === 'boolean') {
+            super(data);
+        }
+        else {
+            throw new Error("O tipo está errado");
+        }
     }
 }
